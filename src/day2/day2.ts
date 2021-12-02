a8861f178284b578694c636568ac782ef0c04a03
Charles Moss <charlesamoss@yahoo.com>
2021-12-02 09:13:11 -0800

    add day one part one

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -1,13 +1,29 @@
 import fs from 'fs';
 
-// const input: string = fs.readFileSync('./data/day1/dayoneInput.txt', 'utf-8')
-// const list: Array<number> = input.toString().replace(/\r\n/g,'\n').split('\n').map( i => +i);
+const input: string = fs.readFileSync('./data/day2/daytwoInput.txt', 'utf-8')
+const list: Array<string> = input.toString().replace(/\r\n/g,'\n').split('\n')
 
 export const partone = () => { 
+    let x: number = 0, y: number = 0;
+   
+    list.map(l => { 
+        let [command, value] = l.split(' ');
+      
+        switch(command) {
+            case 'forward':
+                x = x + <number> +value;
+                break;
+            case 'up':
+                y = y - <number> +value;
+                break;
+            case 'down':
+                y = y + <number> +value;
+                break;
+        }
 
+    })
 
-    
-    return 'hey'
+    return x * y
 }
 
 export const parttwo = () => {
